c69a0d71a6e7d3beb4904eaa47884235b0b9a989
Katherine Baik <112200879+KatherineBaik@users.noreply.github.com>
2025-04-27 16:51:16 -0700

    Added runit to test all the questions

--- family_kinship.pl
+++ family_kinship.pl
@@ -48,23 +48,55 @@ father(X, Y) :- male(X), married(X, Wife), parent(Wife, Y). %If married, and wif
 mother(X, Y) :- female(X), parent(X, Y).
 mother(X, Y) :- female(X), married(Husband, X), parent(Husband, Y). %If married, and husband has son
 
-son(X, Y) :- male(X), parent(Y, X).
-daughter(X, Y) :- female(X), parent(Y, X).
+parent_of(X,Y) :- father(X,Y) ; mother(X,Y). % to fix things?
 
-parent_in_law(X, Y) :- parent(X, Z), married(Z, Y).
-parent_in_law(X, Y) :- parent(X, Z), married(Y, Z).
+sibling(X,Y) :- parent_of(Z, X), parent_of(Z, Y), X \= Y.
+
+son(X, Y) :- male(X), parent_of(Y, X).
+daughter(X, Y) :- female(X), parent_of(Y, X).
+
+parent_in_law(X, Y) :- parent_of(X, Z), married(Z, Y).
+parent_in_law(X, Y) :- parent_of(X, Z), married(Y, Z).
 
 son_in_law(X,Y) :- married(X, Z), daughter(Z, Y).
 
-uncle(X, Y) :- male(X), parent(Z, Y), brother(X, Z).
-aunt(X, Y) :- female(X), parent(Z, Y), sister(X, Z).
+uncle(X, Y) :- male(X), parent_of(Z, Y), brother(X, Z).
+aunt(X, Y) :- female(X), parent_of(Z, Y), sister(X, Z).
 
 sibling_in_law(X, Y) :- married(X, Z), sibling(Z, Y).
 sibling_in_law(X, Y) :- sibling(X, Z), married(Z, Y).
 
-step_parent(X, Y) :- married(X, Z), parent(Z, Y).
+step_parent(X, Y) :- married(X, Z), parent_of(Z, Y).
 
-grandparent(X, Y) :- parent(X, Z), parent(Z, Y).
+grandparent(X, Y) :- parent_of(X, Z), parent_of(Z, Y).
 grandchild(X, Y) :- grandparent(Y, X).
 
-brother(X, Y) :- male(X), parent(Z, X), parent(Z, Y), X \= Y.
+brother(X, Y) :- male(X), parent_of(Z, X), parent_of(Z, Y), X \= Y.
+
+
+% For testing purposes
+% Answer all the questions listed at the start
+
+/* Questions: 
+* Is my father my son-in-law?
+* Is my daughter my step-mother?
+* Is my son my father's brother-in-law?
+* Is my son my uncle?
+* Is my son a brother to my daughter?
+* Is my father's son my grandson?
+* Is my wife my grandmother?
+* Am I my wife's grandchild?
+*
+* Main Question: Am I my own Grandpa? 
+* NOTE: SHOULD ANSWER ALL YES
+*/
+
+runit :- parent_in_law(i, father), 	write(yes), nl,
+	step_parent(i, redhair), 	write(yes), nl,
+	sibling_in_law(babyboy, father),write(yes), nl, %Right now fails, everything else works
+	uncle(babyboy, i), 		write(yes), nl,
+	brother(babyboy, redhair), 	write(yes), nl,
+	grandchild(onrun, i), 		write(yes), nl,
+	grandparent(widow, i), 		write(yes), nl,
+	grandchild(i, widow), 		write(yes), nl,
+	grandparent(i,i), 		write(yes).
